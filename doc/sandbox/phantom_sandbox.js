9719d4e3dd4e2c5af2bfd35a19bdf68b9df40d61
spark <pierre.gayvallet@gmail.com>
2013-03-19 17:47:25 +0100

    updated phantom prez

--- doc/sandbox/phantom_sandbox.js
+++ doc/sandbox/phantom_sandbox.js
@@ -1,8 +1,31 @@
 
 // screencast
 
-var page = require('webpage').create();
+/*var page = require('webpage').create();
+
+page.viewportSize = { width: 480, height: 400 };
+
+// page.clipRect = { top: 14, left: 3, width: 400, height: 300 };
+
 page.open('http://github.com/', function () {
     page.render('github.png');
     phantom.exit();
+});*/
+
+
+// double context
+
+var page = require('webpage').create();
+page.viewportSize = { width: 1280, height: 1024 };
+page.open('http://serpodile.com/', function (status) {
+
+    // document.querySelector("input[name=q]")
+    // $(".produit h2")  // document.querySelectorAll(".produit h2")
+
+    console.log(status);
+    console.log(JSON.stringify(page.viewportSize));
+
+    page.render('test.png');
+    phantom.exit();
 });
+
