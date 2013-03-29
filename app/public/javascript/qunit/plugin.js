e8ba6c6833d9ff8d8f86607eaa6ee03cf4fe3b2f
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2013-03-29 15:51:52 +0100

    Removing app fonder

--- app/public/javascript/qunit/plugin.js
+++
@@ -1,8 +0,0 @@
-QUnit.extend( QUnit.assert, {
-    /**
-     * Asserts that given number is strictly greater than given minimum
-     */
-    greaterThan : function(number, minimum, message) {
-        QUnit.push(number > minimum, number, "> " + minimum, message);
-    }
-});
